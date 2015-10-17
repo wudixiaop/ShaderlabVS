77dfde3327c4997cc620c4fe365d853cb52a7937
Rocky Lai <rockylai@qq.com>
2015-10-17 14:11:46 +0800

    add vs 2015 plugin register tool

---
+++ Tools/vs2015-register.cmd
@@ -0,0 +1,8 @@
+﻿@ECHO OFF
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.shader" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.cginc" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
+
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0Exp_Config\Languages\File Extensions\.shader" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0Exp_Config\Languages\File Extensions\.cginc" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
+
+pause
