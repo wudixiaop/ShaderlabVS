32449fb49bc7405d46b6f7719fa6a3bb0c3bb19e
rocky <rocky@ROCKY671B>
2016-06-18 19:11:30 +0800

    fix bugs #7 and #8

--- Tools/vs2015-register.cmd
+++ Tools/vs2015-register.cmd
@@ -1,8 +1,7 @@
 ﻿@ECHO OFF
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.shader" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.cginc" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
-
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0Exp_Config\Languages\File Extensions\.shader" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0Exp_Config\Languages\File Extensions\.cginc" /t REG_SZ /d {c702cfb7-573c-45f4-9469-115fcb519ad2} /f  
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.shader" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.cginc" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.compute" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.glslinc" /f
 
 pause
