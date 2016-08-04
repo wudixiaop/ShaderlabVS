b749dce895d1dd89958514e5644ea3032143794e
Rocky Lai <wudixiaop@users.noreply.github.com>
2016-08-04 22:39:22 +0800

    update register tools

--- Tools/vs2015-register.cmd
+++
@@ -1,7 +0,0 @@
-﻿@ECHO OFF
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.shader" /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.cginc" /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.compute" /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.glslinc" /f
-
-pause
---
+++ Tools/vs2015_2013_2012-regiser.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+REM 2015
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.shader" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.cginc" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.compute" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config\Languages\File Extensions\.glslinc" /f
+
+REM VS2013
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.shader" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.cginc" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.compute" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.glslinc" /f
+
+REM VS2012
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.shader" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.cginc" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.compute" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0_Config\Languages\File Extensions\.glslinc" /f
+
+pause
